ae9899d4d33acfec9bc3a96e5b72edc7392d4579
wingeherose9145 <121928673+wingeherose9145@users.noreply.github.com>
2026-05-24 15:35:10 +0800

    Update DictionaryEntry.kt

--- app/src/main/java/com/nichudict/DictionaryEntry.kt
+++ app/src/main/java/com/nichudict/DictionaryEntry.kt
@@ -2,6 +2,6 @@ package com.nichudict
 
 data class DictionaryEntry(
     val word: String,
-    val reading: String,
-    val meaning: String
+    val reading: String, // 如果数据库里没这个字段，也可以保留，或者将其置为空
+    val content: String  // 关键：这里直接用 content
 )
